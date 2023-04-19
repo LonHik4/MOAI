9bb9d5d5bf05ca79e9b52efbd04290e14ba841f3
LONHik4 <gaynullin.dmitriy@gmail.com>
2023-04-19 17:10:08 +0400

    Add func additive noise calc

--- 3lab.py
+++ 3lab.py
@@ -9,16 +9,19 @@ def chessboard():
     chess = (np.indices((128, 128)) // 16).sum(axis=0) % 2
     return chess
 
+def additive_noise_calc(orig_img, snr):
+    disp_add_noise = np.var(orig_img) / 255 ** 2
+    additive_noise_img = random_noise(orig_img/255, var=disp_add_noise/snr)*255
+    additive_noise = additive_noise_img - orig_img + 128
+    return additive_noise_img , additive_noise
 
 if __name__ == "__main__":
     board = np.array([160, 96])
     orig_img = (board[chessboard()])
     plt.subplot(221)
     plt.imshow(orig_img, cmap='gray', vmin=0, vmax=255)
-    disp_orig = np.var(orig_img/255)
-    disp_add_noise = np.var(orig_img/255) / 1
-    additive_noise_img = random_noise(orig_img/255, var=disp_add_noise)*255
-    additive_noise = additive_noise_img - orig_img + 128
+    disp_orig = np.var(orig_img)
+    additive_noise_img, additive_noise = additive_noise_calc(orig_img, 1)
     plt.subplot(222)
     plt.imshow(additive_noise, cmap='gray', vmin=0, vmax=255)
     plt.subplot(223)
@@ -27,14 +30,13 @@ if __name__ == "__main__":
 
     plt.subplot(221)
     plt.imshow(orig_img, cmap='gray', vmin=0, vmax=255)
-    disp_add_noise = np.var(orig_img/255) / 10
-    additive_noise_img = random_noise(orig_img/255, var=disp_add_noise)*255
+    disp_add_noise = np.var(orig_img)/255**2
+    additive_noise_img = random_noise(orig_img/255, var=disp_add_noise/10)*255
     additive_noise = additive_noise_img - orig_img + 128
     plt.subplot(222)
     plt.imshow(additive_noise, cmap='gray', vmin=0, vmax=255)
     plt.subplot(223)
     plt.imshow(additive_noise_img, cmap='gray', vmin=0, vmax=255)
     plt.show()
-
 
 
