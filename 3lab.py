3db8070be309e3ccc654d0a8d9fee7b9f3cd569a
LONHik4 <gaynullin.dmitriy@gmail.com>
2023-04-19 17:14:08 +0400

    removed warnings

--- 3lab.py
+++ 3lab.py
@@ -5,15 +5,18 @@ from skimage.util import random_noise
 from scipy.signal import convolve2d
 from scipy.ndimage import median_filter
 
+
 def chessboard():
     chess = (np.indices((128, 128)) // 16).sum(axis=0) % 2
     return chess
 
-def additive_noise_calc(orig_img, snr):
-    disp_add_noise = np.var(orig_img) / 255 ** 2
-    additive_noise_img = random_noise(orig_img/255, var=disp_add_noise/snr)*255
-    additive_noise = additive_noise_img - orig_img + 128
-    return additive_noise_img , additive_noise
+
+def additive_noise_calc(img, snr):
+    disp = np.var(img) / 255 ** 2
+    noise_img = random_noise(img/255, var=disp/snr)*255
+    noise = noise_img - img + 128
+    return noise_img, noise
+
 
 if __name__ == "__main__":
     board = np.array([160, 96])
@@ -38,5 +41,3 @@ if __name__ == "__main__":
     plt.subplot(223)
     plt.imshow(additive_noise_img, cmap='gray', vmin=0, vmax=255)
     plt.show()
-
-
